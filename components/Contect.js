81b48e1672758f2790e42ebdb62a69b28228ea51
prit4848 <pritrajput415@gmail.com>
2024-09-20 23:09:37 +0530

    initialize commit

---
+++ components/Contect.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const Contect = () => {
+  return (
+    <div>
+      <div className="bg-green-600 flex align-middle text-center  py-5 px-5 justify-between h-20">
+        <h4>logo</h4>
+        <nav className="flex gap-5">
+          <Link href='/About'>About</Link>
+          <Link href='/Contact'>Contact</Link>
+          <Link href='/Features'>Features</Link>
+        </nav>
+      </div>
+      <h1>contect</h1>
+    </div>
+  );
+};
+
+export default Contect;
---
+++ pages/Features/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Features() {
+  return (
+    <>
+        <div className="bg-green-600 flex align-middle text-center  py-5 px-5 justify-between h-20">
+         <h4>logo</h4>
+         <nav className="flex gap-5">
+          <Link href='/About'>About</Link>
+          <Link href='/Contect'>Contact</Link>
+          <Link href='/Features'>Features</Link>
+         </nav>
+        </div> 
+        <h1>Features</h1>
+    </>
+  );
+}
